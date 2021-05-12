233a25c83e47a8c0ce2c02344cdaab239bb21a15
gracechan6 <grace502@qq.com>
2021-05-12 15:53:36 +0800

    init

---
+++ code.js
@@ -0,0 +1,176 @@
+/* 
+ * @Description  : input code
+ * @Author       : grace
+ * @Date         : 2021-05-11 17:02:39
+ * @LastEditors  : grace
+ * @LastEditTime : 2021-05-11 18:55:42
+ * @FilePath     : /code.js
+ */
+
+var validCode = 'test';
+
+var day1 = {
+  1: {
+    text: "09:00 Policy Trends to Promote Sustainable Packaging in China",
+    subtext: "促进中国可持续包装的政策发展",
+    download: {
+      record: "1.mp3",
+      ppt: "1.ppt",
+      recordEN: "1.mp3",
+      pptEN: "1.ppt"
+    }
+  },
+  2: {
+    text: "09: 40 The Plastic Cleansing Action - The New Model of Plastic Reduction and Environmental Protection",
+    subtext: "清塑行动 - 减塑新模式环保新时尚",
+    download: {
+      record: "1.mp3",
+      ppt: "1.ppt",
+      recordEN: "1.mp3",
+      pptEN: "1.ppt"
+    }
+  },
+  3: {
+    text: "10: 50 Panel Discussion: How to Combine the Power of Governments, Brands and NGOs to Make Packaging Sustainable",
+    subtext: "小组讨论： 如何联合政府、品牌和社会组织的力量促进包装可持续？",
+    download: {
+      record: "1.mp3",
+      ppt: "1.ppt",
+      recordEN: "1.mp3",
+      pptEN: "1.ppt"
+    }
+  },
+  4: {
+    text: "13: 30 P & G's GC Sustainability Goal and Packaging Innovation",
+    subtext: "宝洁大中华区可持续发展目标和包装创新",
+    download: {
+      record: "1.mp3",
+      ppt: "1.ppt",
+      recordEN: "1.mp3",
+      pptEN: "1.ppt"
+    }
+  },
+  5: {
+    text: "14: 10 Innovation in Packaging: How to Fulfill Sustainability",
+    subtext: "包装创新：如何实现可持续发展",
+    download: {
+      record: "1.mp3",
+      ppt: "1.ppt",
+      recordEN: "1.mp3",
+      pptEN: "1.ppt"
+    }
+  },
+  6: {
+    text: "15: 20 The challenge of the ban on plastics regulation to the dairy industry and packaging innovation",
+    subtext: "限塑、禁塑”调控政策对乳品行业的挑战及包装创新",
+    download: {
+      record: "",
+      ppt: "",
+      recordEN: "",
+      pptEN: ""
+    }
+  },
+  7: {
+    text: "16: 00 Regulations and Technologies Challenges in Driving the Circular Economy",
+    subtext: "推动循环经济的法规和技术挑战",
+    download: {
+      record: "",
+      ppt: "",
+      recordEN: "",
+      pptEN: ""
+    }
+  },
+  8: {
+    text: "16: 40 Panel Discussion: How to Accelerate the Commercialization of Sustainability ?",
+    subtext: "小组讨论 : 如何让可持续发展更加商业化？",
+    download: {
+      record: "",
+      ppt: "",
+      recordEN: "",
+      pptEN: ""
+    }
+  }
+};
+
+var day2 = {
+  1: {
+    text: "09:00 Develop the Circular Economy for Plastic Packaging in China",
+    subtext: "建立全新的塑料包装循环经济",
+    download:{
+      record:"1.mp3",
+      ppt:"1.ppt",
+      recordEN:"1.mp3",
+      pptEN:"1.ppt"
+    }
+  },
+  2: {
+    text: "09:40 New Materials for Sustainable Future - Create a closed-loop ecosystem with sustainable packaging",
+    subtext: "创新材料·绿色未来 - 打造可持续包装闭环生态系统",
+    download:{
+      record:"1.mp3",
+      ppt:"1.ppt",
+      recordEN:"1.mp3",
+      pptEN:"1.ppt"
+    }
+  },
+  3: {
+    text: "10:40 Consumer Insights on Sustainable Packaging",
+    subtext: "消费者洞察",
+    download:{
+      record:"1.mp3",
+      ppt:"1.ppt",
+      recordEN:"1.mp3",
+      pptEN:"1.ppt"
+    }
+  },
+  4: {
+    text: "11:20 Panel Discussion: Paper or Plastic?",
+    subtext: "小组讨论：纸 VS 塑料",
+    download:{
+      record:"1.mp3",
+      ppt:"1.ppt",
+      recordEN:"1.mp3",
+      pptEN:"1.ppt"
+    }
+  },
+  5: {
+    text: "13:30 How to Deliver Sustainability in Flexible Packaging?",
+    subtext: "如何实现软包装的可持续性？",
+    download:{
+      record:"1.mp3",
+      ppt:"1.ppt",
+      recordEN:"1.mp3",
+      pptEN:"1.ppt"
+    }
+  },
+  6: {
+    text: "14:10 Driving Australia’s transition to a Circular Economy for Packaging",
+    subtext: "推动澳大利亚向包装循环经济发展",
+    download:{
+      record:"1.mp3",
+      ppt:"1.ppt",
+      recordEN:"1.mp3",
+      pptEN:"1.ppt"
+    }
+  },
+  7: {
+    text: "15:10 Panel Discussion: Is 100% Recyclable Flexible Packaging Possible?",
+    subtext: "小组讨论:100% 可回收的软包装是否可行?",
+    download:{
+      record:"",
+      ppt:"",
+      recordEN:"",
+      pptEN:""
+    }
+  },
+  8: {
+    text: "16:00 Panel Discussion: The Recycling Realities in China and Real Case Study for packaging recycling",
+    subtext: "小组讨论: 中国的回收现状和包装回收实际案例",
+    download:{
+      record:"",
+      ppt:"",
+      recordEN:"",
+      pptEN:""
+    }
+  }  
+ };
